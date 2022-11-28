97b763c892edf5e375dc1afbb3e1c606b08e3447
Fabian Miguel Canizares <jekoy99@gmail.com>
2022-11-28 23:03:18 +0800

    Create Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { TicketListComponent } from './tickets/ticket-list/ticket-list.component';
+import { CreateTicketComponent } from './tickets/create-ticket/create-ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    TicketsComponent,
+    TicketListComponent,
+    CreateTicketComponent
   ],
   imports: [
     BrowserModule,
