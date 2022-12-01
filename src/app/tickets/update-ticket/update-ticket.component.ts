8d310a1c26a4087e7b400c43e47658328fd788cf
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2022-12-02 02:55:01 +0800

    Finished update ticket

--- src/app/tickets/update-ticket/update-ticket.component.ts
+++ src/app/tickets/update-ticket/update-ticket.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-ticket.component.css']
 })
 export class UpdateTicketComponent {
+  
+  status:boolean = false;
+  assignee:boolean = false;
+  ticket:boolean = false;
 
+  onClick(toggle : string) {
+    if (toggle == "status") {
+      this.status = !this.status;
+      this.assignee = false;
+      this.ticket = false;
+    } else if (toggle == "assignee") {
+      this.assignee = !this.assignee;
+      this.status = false;
+      this.ticket = false;
+    } else {
+      this.ticket = !this.ticket;
+      this.status = false;
+      this.assignee = false;
+    }
+    // this.status= !this.status;
+    // 
+    // this.ticket = !this.ticket;
+  }
 }
