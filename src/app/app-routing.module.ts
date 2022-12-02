399b8c51cfb2432eced9e04a7d0fbaf2b9f3e0e3
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2022-12-02 21:00:45 +0800

    moved the modal style in a global css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     path: '',
     component: DashboardPageComponent,
     children: [
-      {
-        path: 'ticketlist',
-        component: TicketListComponent,
-      },
       
 
     ],
