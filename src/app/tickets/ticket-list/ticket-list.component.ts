34377bb80b6b1d8904571c5916d40fcbf9a8b170
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-12-05 21:10:26 +0800

    Connect Ticket List Data on Database

--- src/app/tickets/ticket-list/ticket-list.component.ts
+++ src/app/tickets/ticket-list/ticket-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-list',
+  providers: [TicketService],
   templateUrl: './ticket-list.component.html',
   styleUrls: ['./ticket-list.component.css']
 })
 export class TicketListComponent {
+
+  constructor(private ticketService: TicketService){}
+
+  tickets$: any[] = [];
+
+  ngOnInit(): void {
+    this.ticketService.getAllTickets().subscribe((result) => {
+      this.tickets$ = result['data'];
+    })
+  }
 
 }
