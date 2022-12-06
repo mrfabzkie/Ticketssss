23e676a6784a075457f11088cf35333416524125
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-12-06 15:17:06 +0800

    Partial UI Update on TicketList with Backend

--- src/app/tickets/ticket-list/ticket-list.component.ts
+++ src/app/tickets/ticket-list/ticket-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Ticket } from 'src/app/models/ticket';
 import { TicketService } from 'src/app/services/ticket.service';
+import { ViewTicketComponent } from '../view-ticket/view-ticket.component';
 
 @Component({
   selector: 'app-ticket-list',
@@ -8,8 +11,18 @@ import { TicketService } from 'src/app/services/ticket.service';
   styleUrls: ['./ticket-list.component.css']
 })
 export class TicketListComponent {
+  
+  viewedTicket : Ticket = {
+    ticketID: 0,
+    status: 'NEW',
+    subject: 'SUBJ',
+    description: '',
+    tracker: '',
+    assignee: 0,
+    requester: 0
+  }
 
-  constructor(private ticketService: TicketService){}
+  constructor(private ticketService: TicketService, private dialog: MatDialog, ){}
 
   tickets$: any[] = [];
 
@@ -17,6 +30,16 @@ export class TicketListComponent {
     this.ticketService.getAllTickets().subscribe((result) => {
       this.tickets$ = result['data'];
     })
+  }
+
+  onClickView(i: number){
+    this.viewedTicket.ticketID = this.tickets$[i]['ticketID'];
+    this.viewedTicket.tracker = this.tickets$[i]['tracker'];
+    this.viewedTicket.assignee = this.tickets$[i]['assignee'];
+    this.viewedTicket.subject = this.tickets$[i]['subject'];
+    this.viewedTicket.description = this.tickets$[i]['description'];
+
+    this.dialog.open(ViewTicketComponent, {});
   }
 
 }
