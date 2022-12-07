171e15537cc0afda2436c8da2b066d5723d16c78
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2022-12-07 23:05:55 +0800

    added delete component

---
+++ src/app/tickets/delete-ticket/delete-ticket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-ticket',
+  templateUrl: './delete-ticket.component.html',
+  styleUrls: ['./delete-ticket.component.scss']
+})
+export class DeleteTicketComponent {
+
+}
