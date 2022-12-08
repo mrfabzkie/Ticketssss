03ba33457225a35a0c6524b2c0d06bad89d89c8d
Fabian Miguel Canizares <jekoy99@gmail.com>
2022-12-09 00:42:47 +0800

    test

--- src/app/users-page/users-list/users-list.component.ts
+++ src/app/users-page/users-list/users-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TicketService } from 'src/app/services/ticket.service';
+import { TicketStatusService } from 'src/app/services/ticketstatus.service';
+import { TrackerService } from 'src/app/services/tracker.service';
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
+  providers: [TicketService],
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnChanges, OnInit {
+
+  users$ : any []=[];
+
+  constructor(
+    private userService: UserService,
+  ) {}
+  ngOnInit(): void {
+   
+  }
+  
+  
+  ngOnChanges(changes: SimpleChanges): void {
+   this.userService.getAllSearchedUsers("","").subscribe((result) => {
+    this.users$ = result['data'];
+    console.log(this.users$);
+  });
+  }
+
 
 }
