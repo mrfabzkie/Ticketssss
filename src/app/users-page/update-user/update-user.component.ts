be979bcdc58f0d51d7cc459ef5f10d839271f686
Fabian Miguel Canizares <jekoy99@gmail.com>
2022-12-08 22:44:29 +0800

    partial modal screen in update-user page

--- src/app/users-page/update-user/update-user.component.ts
+++ src/app/users-page/update-user/update-user.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-update-user',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-user.component.scss']
 })
 export class UpdateUserComponent {
+  @Input() updatedTicket: any;
 
+  @Output() updatedStatus = new EventEmitter<boolean>();
+
+  onClose(){
+    this.updatedStatus.emit(false);
+  }
+
+  status:boolean = true;
+  assignee:boolean = false;
+  ticket:boolean = false;
+
+  onClick(toggle : string) {
+    if (toggle == "status") {
+      this.status = !this.status;
+      this.assignee = false;
+      this.ticket = false;
+    } else if (toggle == "assignee") {
+      this.assignee = !this.assignee;
+      this.status = false;
+      this.ticket = false;
+    } else {
+      this.ticket = !this.ticket;
+      this.status = false;
+      this.assignee = false;
+    }
+  }
 }
