c86faa5b52b6e990fca54ed348a6ea0150eaee0e
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-12-08 14:03:02 +0800

    Add TicketListType and data bind

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -7,6 +7,12 @@ import { TrackerService } from '../services/tracker.service';
 import { TicketStatusService } from '../services/ticketstatus.service';
 import { UserService } from '../services/user.service';
 
+enum TicketListType {
+  All,
+  Active,
+  Aging,
+}
+
 @Component({
   selector: 'app-dashboard-page',
   providers: [TrackerService, TicketStatusService, UserService],
@@ -20,26 +26,28 @@ export class DashboardPageComponent {
     private fb: FormBuilder,
     private trackerService: TrackerService,
     private ticketStatusService: TicketStatusService,
-    private userService: UserService,
+    private userService: UserService
   ) {}
 
-  ngOnInit(){
-    this.form.get('searchedValue')!.valueChanges.subscribe(result => {
+  ngOnInit() {
+    this.form.get('searchedValue')!.valueChanges.subscribe((result) => {
       this.filters['searchedValue'] = result;
       this.searchedValue = result!;
     });
 
-    this.trackerService.getAllTrackers().subscribe(result => {
+    this.trackerService.getAllTrackers().subscribe((result) => {
       this.trackers$ = result['data'];
     });
 
-    this.ticketStatusService.getAllTicketStatus().subscribe(result =>{
+    this.ticketStatusService.getAllTicketStatus().subscribe((result) => {
       this.ticketStatus$ = result['data'];
     });
 
-    this.userService.getAllUsers().subscribe(result => {
+    this.userService.getAllUsers().subscribe((result) => {
       this.users$ = result['data'];
-    })
+    });
+
+    this.changeTicketListTypeButtonLabel();
   }
 
   isCreating: boolean = false;
@@ -48,12 +56,14 @@ export class DashboardPageComponent {
   trackers$: any[] = [];
   ticketStatus$: any[] = [];
   users$: any[] = [];
+  showTicketListType = TicketListType.All;
+  TicketListTypeButtonLabel = '';
 
   filters: any = {
     searchedValue: '',
     tracker: '',
     status: '',
-  }
+  };
 
   searchedValue: String = '';
   tracker: String = '';
@@ -62,7 +72,7 @@ export class DashboardPageComponent {
   form = this.fb.group({
     searchedValue: [''],
     tracker: [''],
-    status: ['']
+    status: [''],
   });
 
   onCreateTicket() {
@@ -85,13 +95,30 @@ export class DashboardPageComponent {
     return this.form.controls;
   }
 
-  changeTracker(value: any){
+  changeTracker(value: any) {
     this.tracker = value.target.value;
     this.filters['tracker'] = value.target.value;
   }
 
-  changeStatus(value: any){
+  changeStatus(value: any) {
     this.status = value.target.value;
     this.filters['status'] = value.target.value;
+  }
+
+  changeShowTicketListType() {
+    let index = this.showTicketListType;
+    this.showTicketListType =
+      this.showTicketListType == Object.keys(TicketListType).length / 2 - 1
+        ? 0
+        : index + 1;
+
+    this.changeTicketListTypeButtonLabel();
+  }
+
+  changeTicketListTypeButtonLabel() {
+    this.TicketListTypeButtonLabel =
+      this.showTicketListType == Object.keys(TicketListType).length / 2 - 1
+        ? TicketListType[0]
+        : TicketListType[this.showTicketListType + 1];
   }
 }
