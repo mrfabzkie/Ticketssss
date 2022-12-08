f1c2bc6bfa1d101f25e42e874285fcc58cc83da0
Fabian Miguel Canizares <jekoy99@gmail.com>
2022-12-08 22:51:41 +0800

    partial modal screen in delete-user page

--- src/app/users-page/delete-user/delete-user.component.ts
+++ src/app/users-page/delete-user/delete-user.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { TicketService } from 'src/app/services/ticket.service';
+
 
 @Component({
   selector: 'app-delete-user',
   templateUrl: './delete-user.component.html',
-  styleUrls: ['./delete-user.component.scss']
+  styleUrls: ['./delete-user.component.scss'],
+  providers: [TicketService],
 })
 export class DeleteUserComponent {
 
+  @Input() deletedTicket: any;
+
+  @Output() deleteStatus = new EventEmitter<boolean>();
+
+  onClose(){
+    this.deleteStatus.emit(false);
+  }
 }
